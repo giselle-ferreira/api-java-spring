00cf902253389b44e2ab9f887af081eae6c80a92
Giselle Ferreira <giselle.fs@gmail.com>
2022-11-27 01:39:15 -0300

    Initial Commit | Create User and Department classes

---
+++ src/main/java/com/devsuperior/apijavaspring/ApiJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.apijavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/apijavaspring/ApiJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.apijavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
